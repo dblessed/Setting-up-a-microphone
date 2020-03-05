a46518839566b166e39fc56ededc18a93af35357
dblessed <48085252+dblessed@users.noreply.github.com>
2020-03-05 15:45:36 +0300

    Update microphonerecord.py

--- microphonerecord.py
+++ microphonerecord.py
@@ -1,31 +1,45 @@
 import pyaudio
+import wave
+from array import array
+
+FORMAT=pyaudio.paInt16
+CHANNELS=1
+RATE=44100
+CHUNK=1024
+RECORD_SECONDS=14400
+FILE_NAME="RECORDING.wav"
+
+audio=pyaudio.PyAudio() #instantiate the pyaudio
+
+#recording prerequisites
+stream=audio.open(format=FORMAT,channels=CHANNELS, 
+                  rate=RATE,
+                  input=True,
+                  frames_per_buffer=CHUNK)
+
+#starting recording
+frames=[]
+
+for i in range(0,int(RATE/CHUNK*RECORD_SECONDS)):
+    data=stream.read(CHUNK)
+    data_chunk=array('h',data)
+    vol=max(data_chunk)
+    if(vol>=500):
+        print("something said")
+        frames.append(data)
+    else:
+        print("nothing")
+    print("\n")
 
 
-CHUNK = 1024
-FORMAT = pyaudio.paInt16
-CHANNELS = 1
-RATE = 44100
-RECORD_SECONDS = 5
-WAVE_OUTPUT_FILENAME = "output.wav"
-
-p = pyaudio.PyAudio()
-
-stream = p.open(format=FORMAT,
-                channels=CHANNELS,
-                rate=RATE,
-                input=True,
-                frames_per_buffer=CHUNK)
-
-print("* recording")
-
-frames = []
-
-for i in range(0, int(RATE / CHUNK * RECORD_SECONDS)):
-    data = stream.read(CHUNK)
-    frames.append(data)
-
-print("* done recording")
-
+#end of recording
 stream.stop_stream()
 stream.close()
-p.terminate()
+audio.terminate()
+#writing to file
+wavfile=wave.open(FILE_NAME,'wb')
+wavfile.setnchannels(CHANNELS)
+wavfile.setsampwidth(audio.get_sample_size(FORMAT))
+wavfile.setframerate(RATE)
+wavfile.writeframes(b''.join(frames))#append frames recorded to file
+wavfile.close()
